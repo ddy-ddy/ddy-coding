2f3838ab138d16fcb269c9fcb1ab1577f38e70f7
ddy <58058861+ddy-ddy@users.noreply.github.com>
2023-11-29 18:23:22 +0800

    feat: add shadcn

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+/*
+ * @Author: ddy-ddy
+ * @Date: 2023-11-29 17:57:20
+ * @LastEditTime: 2023-11-29 18:19:48
+ * @Github: https://github.com/ddy-ddy
+ * @Website: https://ddy-ddy.com
+ */
+import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,8 +14,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		alias: {
+			$lib: "./src/lib"
+		}
+	},
+	preprocess: [vitePreprocess({})],
 };
 
 export default config;
