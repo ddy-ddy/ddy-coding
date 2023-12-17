085f479261f6cc06e7a27dcb8af8999bb080050c
ddy <58058861+ddy-ddy@users.noreply.github.com>
2023-12-17 20:58:46 +0800

    feat: delete some unuseful file

--- src/lib/components/page/index.ts
+++ src/lib/components/page/index.ts
@@ -1,6 +1,3 @@
 export { default as Search } from "./search.svelte";
 export { default as SiteHeader } from "./site-header.svelte";
 export { default as SiteFooter } from "./site-footer.svelte";
-export { default as Tree } from "./tree.svelte";
-export { default as TableOfContents } from "./table-of-contents.svelte";
-export { default as BlogSidebarNav } from "./blogs-sidebar-nav.svelte";
--- src/routes/pictures/+layout.svelte
+++ src/routes/pictures/+layout.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-  import { BlogSidebarNav } from "$lib/components/page";
   import { docsConfig } from "$lib/config/blogs";
 </script>
 
 <div class="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] lg:grid-cols-[240px_minmax(0,1fr)] ">
   <aside class="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block overflow-y-auto">
     <div class="h-full py-6 pl-8 pr-6 lg:py-8">
-      <BlogSidebarNav items={docsConfig.sidebarNav} />
+      <!-- <BlogSidebarNav items={docsConfig.sidebarNav} /> -->
     </div>
   </aside>
   <slot />
