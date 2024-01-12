1c3ddd7723aa9e63a81d366a83a7ef131d349b0d
ddy <58058861+ddy-ddy@users.noreply.github.com>
2024-01-12 23:10:02 +0800

    feat: adjust the text color with foreground/80,70,60

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,13 +31,13 @@
         </div>
         <div class="flex space-x-2">
           <a class="group flex space-x-1 items-end" href="https://github.com/ddy-ddy?tab=followers">
-            <Users class="h-4 w-4 group-hover:text-avocado-400 dark:group-hover:text-avocado-600"></Users>
-            <div class="-mb-[1px] text-xs font-medium text-foreground/80 group-hover:text-avocado-400 dark:group-hover:text-avocado-600">{githubInfo.authorGithubFollowers} followers</div>
+            <Users class="h-4 w-4 stroke-foreground/70 group-hover:text-avocado-400 dark:group-hover:text-avocado-600"></Users>
+            <div class="-mb-[1px] text-xs font-medium text-foreground/70 group-hover:text-avocado-400 dark:group-hover:text-avocado-600">{githubInfo.authorGithubFollowers} followers</div>
           </a>
           <Separator orientation="vertical" />
           <div class="flex space-x-1 items-end">
-            <MapPinned class="h-4 w-4"></MapPinned>
-            <div class="-mb-[1px] text-xs font-medium text-foreground/80">{githubInfo.authorGithubLocation}</div>
+            <MapPinned class="h-4 w-4 stroke-foreground/70"></MapPinned>
+            <div class="-mb-[1px] text-xs font-medium text-foreground/70">{githubInfo.authorGithubLocation}</div>
           </div>
         </div>
         <Separator />
@@ -47,7 +47,7 @@
           {#each siteConfig.codingInfo as codingInfo}
             <div class="flex items-center space-x-1">
               <BadgeCheck class="h-4 w-4 {codingInfo.color}" />
-              <p class="text-xs font-medium text-foreground/80">{codingInfo.name}</p>
+              <p class="text-xs font-medium text-foreground/70">{codingInfo.name}</p>
             </div>
           {/each}
         </div>
@@ -57,15 +57,15 @@
         <div class="flex flex-wrap gap-x-3 gap-y-3">
           <a href="/pictures" class="group flex space-x-1 border bg-card rounded-lg shadow-sm shadow-avocado-400 dark:shadow-avocado-800 p-1">
             <Camera class="group-hover:animate-move-right-left h-4 w-4 fill-avocado-200 dark:fill-avocado-600"></Camera>
-            <p class="text-xs font-medium text-foreground/80">摄影</p>
+            <p class="text-xs font-medium text-foreground/70">摄影</p>
           </a>
           <a href="https://www.strava.com/athletes/duanyu" target="_blank" class="group flex space-x-1 border bg-card rounded-lg shadow-sm shadow-orange-400 dark:shadow-orange-800 p-1">
             <Bike class="group-hover:animate-move-right-left h-4 w-4 fill-orange-200 dark:fill-orange-600"></Bike>
-            <p class="text-xs font-medium text-foreground/80">骑行</p>
+            <p class="text-xs font-medium text-foreground/70">骑行</p>
           </a>
           <a href="https://space.bilibili.com/434180761" target="_blank" class="group flex space-x-1 border bg-card rounded-lg shadow-sm shadow-sky-400 dark:shadow-sky-800 p-1">
             <PiggyBank class="group-hover:animate-move-right-left h-4 w-4 fill-sky-200 dark:fill-sky-600"></PiggyBank>
-            <p class="text-xs font-medium text-foreground/80">UP主</p>
+            <p class="text-xs font-medium text-foreground/70">UP主</p>
           </a>
         </div>
         <Separator />
@@ -76,7 +76,7 @@
             <Drawer.Trigger>
               <div class="group flex space-x-1 border bg-card rounded-lg shadow-sm py-1 px-2">
                 <Icons.wechat class="group-hover:animate-move-right-left h-4 w-4"></Icons.wechat>
-                <p class="text-xs font-medium text-foreground/80">WeChat</p>
+                <p class="text-xs font-medium text-foreground/70">WeChat</p>
               </div>
             </Drawer.Trigger>
             <Drawer.Content>
@@ -90,15 +90,15 @@
           </Drawer.Root>
           <a href="https://space.bilibili.com/434180761" target="_blank" class="group flex space-x-1 border bg-card rounded-lg shadow-sm py-1 px-2">
             <Icons.bilibili class="group-hover:animate-move-right-left h-4 w-4"></Icons.bilibili>
-            <p class="text-xs font-medium text-foreground/80">Bilibili</p>
+            <p class="text-xs font-medium text-foreground/70">Bilibili</p>
           </a>
           <a href="https://twitter.com/ddy_coding" target="_blank" class="group flex space-x-1 border bg-card rounded-lg shadow-sm py-1 px-2">
             <Icons.twitter class="group-hover:animate-move-right-left h-4 w-4 p-[2px]"></Icons.twitter>
-            <p class="text-xs font-medium text-foreground/80">Twitter</p>
+            <p class="text-xs font-medium text-foreground/70">Twitter</p>
           </a>
           <a href="https://github.com/ddy-ddy" target="_blank" class="group flex space-x-1 border bg-card rounded-lg shadow-sm py-1 px-2">
             <Icons.github class="group-hover:animate-move-right-left h-4 w-4 p-[1px]"></Icons.github>
-            <p class="text-xs font-medium text-foreground/80">GitHub</p>
+            <p class="text-xs font-medium text-foreground/70">GitHub</p>
           </a>
         </div>
       </div>
@@ -118,23 +118,23 @@
             {#each githubInfo.authorGithubRepo as repo}
               <div class="flex flex-col space-y-3 w-full rounded-xl border bg-card text-card-foreground shadow p-4">
                 <a href={repo.link} target="_blank" class="flex items-center space-x-2 group">
-                  <Album class="h-4 w-4 stroke-avocado-400 dark:stroke-avocado-600"></Album>
-                  <div class="text-sm font-bold text-avocado-400 dark:text-avocado-600">{repo.name}</div>
+                  <Album class="h-4 w-4 stroke-foreground/70 group-hover:stroke-avocado-400 dark:group-hover:stroke-avocado-600"></Album>
+                  <div class="text-sm font-bold text-foreground/70 group-hover:text-avocado-400 dark:group-hover:text-avocado-600">{repo.name}</div>
                 </a>
-                <p class="text-xs font-medium text-foreground/80">{repo.description}</p>
+                <p class="text-xs font-medium text-foreground/70">{repo.description}</p>
                 <div class="flex space-x-6">
                   <a href={repo.link} target="_blank" class="group flex space-x-1">
-                    <Star class="h-4 w-4 group-hover:stroke-avocado-400 dark:group-hover:stroke-avocado-600"></Star>
-                    <p class="text-xs font-medium text-foreground/80 group-hover:text-avocado-400 dark:group-hover:text-avocado-600">{repo.star}</p>
+                    <Star class="h-4 w-4 stroke-foreground/60 group-hover:stroke-avocado-400 dark:group-hover:stroke-avocado-600"></Star>
+                    <p class="text-xs font-medium text-foreground/60 group-hover:text-avocado-400 dark:group-hover:text-avocado-600">{repo.star}</p>
                   </a>
                   <a href={repo.link} target="_blank" class="group flex space-x-1">
-                    <GitFork class="h-4 w-4 group-hover:stroke-avocado-400 dark:group-hover:stroke-avocado-600"></GitFork>
-                    <p class="text-xs font-medium text-foreground/80 group-hover:text-avocado-400 dark:group-hover:text-avocado-600">{repo.fork}</p>
+                    <GitFork class="h-4 w-4 stroke-foreground/60 group-hover:stroke-avocado-400 dark:group-hover:stroke-avocado-600"></GitFork>
+                    <p class="text-xs font-medium text-foreground/60 group-hover:text-avocado-400 dark:group-hover:text-avocado-600">{repo.fork}</p>
                   </a>
                   {#if repo.language != null}
                     <a href={repo.link} target="_blank" class="group flex space-x-1">
-                      <Languages class="h-4 w-4 group-hover:stroke-avocado-400 dark:group-hover:stroke-avocado-600"></Languages>
-                      <p class="text-xs font-medium text-foreground/80 group-hover:text-avocado-400 dark:group-hover:text-avocado-600">{repo.language}</p>
+                      <Languages class="h-4 w-4 stroke-foreground/60 group-hover:stroke-avocado-400 dark:group-hover:stroke-avocado-600"></Languages>
+                      <p class="text-xs font-medium text-foreground/60 group-hover:text-avocado-400 dark:group-hover:text-avocado-600">{repo.language}</p>
                     </a>
                   {/if}
                 </div>
