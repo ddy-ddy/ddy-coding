5332ea79ab943c8b37c7d656389b41d689a662fb
ddy-ddy <1179730251@qq.com>
2024-01-20 01:32:20 +0800

    feat: change the pictures logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,7 @@
   import { ModeWatcher } from "mode-watcher";
   import { SiteHeader, SiteFooter } from "$lib/components/page";
   import { Toaster } from "$lib/components/ui/sonner";
-  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
-  import { dev } from "$app/environment";
-  import { inject } from "@vercel/analytics";
   
-  inject({ mode: dev ? 'development' : 'production' });
-  injectSpeedInsights();
 </script>
 
 <ModeWatcher />
--- src/routes/pictures/+page.ts
+++ src/routes/pictures/+page.ts
@@ -9,25 +9,27 @@ export const load: PageLoad = async (data: any) => {
         const response = await fetchData(urlBase + "/api/pictures?filters[flag][$eq]=true&populate=*");
         const picturesInfo = response.data;
 
-        const pictures = picturesInfo.map((picture: any) => {
-            return {
-                description: picture.attributes.description,
-                img_url: urlBase + picture["attributes"]["img"]["data"][0]["attributes"]["url"],
-                thumbnail_url: urlBase + picture["attributes"]["img"]["data"][0]["attributes"]["formats"]["thumbnail"]["url"],
-                small_url: urlBase + picture["attributes"]["img"]["data"][0]["attributes"]["formats"]["small"]["url"],
-                medium_url: urlBase + picture["attributes"]["img"]["data"][0]["attributes"]["formats"]["medium"]["url"],
-                large_url: urlBase + picture["attributes"]["img"]["data"][0]["attributes"]["formats"]["large"]["url"],
-                create_time: picture.attributes.exif_info.datetime,
-                camera_make: picture.attributes.exif_info.camera_make,
-                len_make: picture.attributes.exif_info.len_make,
-                len_model: picture.attributes.exif_info.len_model,
-                f_number: picture.attributes.exif_info.f_number,
-                exposure_time: picture.attributes.exif_info.exposure_time,
-                latitude: picture.attributes.exif_info.latitude,
-                longitude: picture.attributes.exif_info.longitude
+        const pictures: any = [];
+        picturesInfo.map((picture: any) => {
+            if (picture.attributes.exif_info.latitude && picture.attributes.exif_info.longitude) {
+                pictures.push({
+                    img_url: urlBase + picture["attributes"]["img"]["data"]["attributes"]["url"],
+                    thumbnail_url: urlBase + picture["attributes"]["img"]["data"]["attributes"]["formats"]["thumbnail"]["url"],
+                    small_url: urlBase + picture["attributes"]["img"]["data"]["attributes"]["formats"]["small"]["url"],
+                    medium_url: urlBase + picture["attributes"]["img"]["data"]["attributes"]["formats"]["medium"]["url"],
+                    large_url: urlBase + picture["attributes"]["img"]["data"]["attributes"]["formats"]["large"]["url"],
+                    description: picture.attributes.exif_info.description,
+                    create_time: picture.attributes.exif_info.datetime,
+                    camera_make: picture.attributes.exif_info.camera_make,
+                    len_make: picture.attributes.exif_info.len_make,
+                    len_model: picture.attributes.exif_info.len_model,
+                    f_number: picture.attributes.exif_info.f_number,
+                    exposure_time: picture.attributes.exif_info.exposure_time,
+                    latitude: picture.attributes.exif_info.latitude,
+                    longitude: picture.attributes.exif_info.longitude
+                })
             }
         });
-
 
         // 处理聚类数据
         var points: any = [];
@@ -36,6 +38,7 @@ export const load: PageLoad = async (data: any) => {
             var picUrl = pictures[i]["large_url"];
             points.push({ lnglat: center, url: picUrl });
         }
+
 
         return {
             pictures: pictures,
