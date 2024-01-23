cf81726969e9b2754fd2ca2f41ccd18b6e612600
ddy-ddy <1179730251@qq.com>
2024-01-23 15:53:13 +0800

    feat: add seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,11 @@
   
 </script>
 
+<svelte:head>
+	<title>ddy的个人网站</title>
+	<meta name="description" content="这是ddy的个人网站，在这里可以查看我的简介，博客，摄影以及骑行信息" />
+</svelte:head>
+
 <ModeWatcher />
 <Toaster />
 <div class="relative flex flex-col min-h-screen">
